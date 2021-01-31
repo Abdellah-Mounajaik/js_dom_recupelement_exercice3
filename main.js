565eda34d93f1200921095416dd42e0183e7ad2a
Abdellah <mounajaikabdellah@gmail.com>
2021-01-31 19:30:29 +0100

    exo_fini

---
+++ main.js
@@ -0,0 +1,20 @@
+var div = document.getElementsByTagName("div")[2].firstElementChild
+console.log(div);
+
+var div = document.getElementsByTagName("div")[2].lastElementChild
+console.log(div);
+
+var div = document.getElementsByTagName("div")[2].querySelector("i")
+console.log(div);
+
+var div = document.getElementsByTagName("div")[2].querySelector("b")
+console.log(div);
+
+var div = document.getElementsByTagName("div")[2].querySelector("i").parentElement
+console.log(div);
+
+var div = document.getElementsByTagName("div")[2].querySelector("b").parentElement
+console.log(div);
+
+var div = document.getElementsByTagName("div")[2].firstElementChild.nextElementSibling
+console.log(div);
